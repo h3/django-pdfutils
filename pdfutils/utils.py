e92304c2c952a902c6461787aec2cb269595b738
Pierre Dulac <dulacpier@gmail.com>
2013-12-08 13:09:37 +0100

    Let pisa handle remote resources

--- pdfutils/utils.py
+++ pdfutils/utils.py
@@ -41,8 +41,11 @@ def fetch_resources(uri, rel):
     Callback to allow xhtml2pdf/reportlab to retrieve Images,Stylesheets, etc.
     `uri` is the href attribute from the html link element.
     `rel` gives a relative path, but it's not used here.
-
+    
     """
+    if uri.startswith('http://') or uri.startswith('https://'):
+        return uri
+        
     if uri.startswith(settings.MEDIA_URL):
         path = os.path.join(settings.MEDIA_ROOT,
                             uri.replace(settings.MEDIA_URL, ""))
