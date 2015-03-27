df4eb5ad6bcc4789d91e0650237c120ab5c6bc26
Maxime Haineault <haineault@gmail.com>
2015-03-27 00:02:27 -0400

    Version bump: 1.0.4 -> 1.0.5 @wip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDFutils',
-    version='1.0.4',
+    version='1.0.5',
     description='Django PDF utils',
     author='Maxime Haineault',
     author_email='max@motion-m.ca',
